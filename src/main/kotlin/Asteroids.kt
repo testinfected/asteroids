36b257c8d0ea1a72cb2cacdbce40ee5b7135a5a4
testinfected <vtence@gmail.com>
2021-04-18 14:10:51 -0400

    Make multiple asteroids move on screen simultaneously
    We'll still learning how JavaFX works.
    Code needs cleanup and we still haven't got any test yet.

--- src/main/kotlin/Asteroids.kt
+++ src/main/kotlin/Asteroids.kt
@@ -4,18 +4,21 @@ import javafx.geometry.Point2D
 import javafx.scene.Group
 import javafx.scene.Scene
 import javafx.scene.paint.Color
-import javafx.scene.shape.Circle
 import javafx.scene.shape.Rectangle
 import javafx.scene.transform.Rotate
+import javafx.scene.transform.Transform
 import javafx.stage.Stage
 import kotlin.random.Random
+
+
+class Asteroid(var pos: Point2D, var angle: Transform)
+
 
 class Asteroids : Application() {
 
     private val grey = Color.rgb(40, 40, 50)
 
-    val angle = Rotate(Random.nextDouble().times(360))
-    val velocity = Point2D(1.5, 0.0)
+    private val velocity = Point2D(1.5, 0.0)
 
     override fun start(stage: Stage) {
         stage.title = "Asteroids"
@@ -24,30 +27,37 @@ class Asteroids : Application() {
         val scene = Scene(root, 800.0, 600.0, grey)
         stage.scene = scene
 
-        var pos = Point2D(200.0, 300.0)
+        val asteroids = (1..5).map {
+            Asteroid(
+                Point2D(Random.nextDouble() * scene.width, Random.nextDouble() * scene.height),
+                Rotate(Random.nextDouble().times(360))
+            )
+        }.toList()
 
         val timer = object : AnimationTimer() {
             override fun handle(now: Long) {
                 root.children.clear()
 
-                val step = angle.transform(velocity)
-                pos = pos.add(step)
-                if (pos.x > scene.width) pos = pos.subtract(scene.width, 0.0)
-                if (pos.x < 0) pos = pos.add(scene.width, 0.0)
-                if (pos.y > scene.height) pos = pos.subtract(0.0, scene.height)
-                if (pos.y < 0) pos = pos.add(0.0, scene.height)
+                for (asteroid in asteroids) {
+                    val step = asteroid.angle.transform(velocity)
+                    asteroid.pos = asteroid.pos.add(step)
+                    if (asteroid.pos.x > scene.width) asteroid.pos = asteroid.pos.subtract(scene.width, 0.0)
+                    if (asteroid.pos.x < 0) asteroid.pos = asteroid.pos.add(scene.width, 0.0)
+                    if (asteroid.pos.y > scene.height) asteroid.pos = asteroid.pos.subtract(0.0, scene.height)
+                    if (asteroid.pos.y < 0) asteroid.pos = asteroid.pos.add(0.0, scene.height)
 
-                val circle = Rectangle(pos.x, pos.y, 120.0, 120.0).apply {
-                    fill = grey
-                    stroke = Color.WHITE
-                    strokeWidth = 2.0
+                    val shape = Rectangle(asteroid.pos.x, asteroid.pos.y, 120.0, 120.0).apply {
+                        fill = grey
+                        stroke = Color.WHITE
+                        strokeWidth = 2.0
+                    }
+
+                    root.children.add(shape)
                 }
-
-                root.children.add(circle)
             }
         }
-        timer.start()
 
+        timer.start()
         stage.show()
     }
 
