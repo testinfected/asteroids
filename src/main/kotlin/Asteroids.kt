75ab1cf2c6b7a24a10b458b81a9c118c5b0aa2d2
testinfected <vtence@gmail.com>
2021-04-18 13:52:08 -0400

    Make asteroid a rectangle moving at random angle
    Asteroid is now a rectangle rather than a circle and moves
    at a random angle.

--- src/main/kotlin/Asteroids.kt
+++ src/main/kotlin/Asteroids.kt
@@ -1,17 +1,21 @@
 import javafx.animation.AnimationTimer
 import javafx.application.Application
+import javafx.geometry.Point2D
 import javafx.scene.Group
 import javafx.scene.Scene
 import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
+import javafx.scene.shape.Rectangle
+import javafx.scene.transform.Rotate
 import javafx.stage.Stage
+import kotlin.random.Random
 
 class Asteroids : Application() {
 
     private val grey = Color.rgb(40, 40, 50)
 
-    var x: Double = 200.0
-    var y: Double = 300.0
+    val angle = Rotate(Random.nextDouble().times(360))
+    val velocity = Point2D(1.5, 0.0)
 
     override fun start(stage: Stage) {
         stage.title = "Asteroids"
@@ -20,22 +24,25 @@ class Asteroids : Application() {
         val scene = Scene(root, 800.0, 600.0, grey)
         stage.scene = scene
 
+        var pos = Point2D(200.0, 300.0)
+
         val timer = object : AnimationTimer() {
             override fun handle(now: Long) {
                 root.children.clear()
 
-                x++
-                if (x > scene.width) x -= scene.width
-                if (x < 0) x += scene.width
-                y += 2
-                if (y > scene.height) y -= scene.height
-                if (y < 0) y+= scene.height
+                val step = angle.transform(velocity)
+                pos = pos.add(step)
+                if (pos.x > scene.width) pos = pos.subtract(scene.width, 0.0)
+                if (pos.x < 0) pos = pos.add(scene.width, 0.0)
+                if (pos.y > scene.height) pos = pos.subtract(0.0, scene.height)
+                if (pos.y < 0) pos = pos.add(0.0, scene.height)
 
-                val circle = Circle(x, y, 60.0).apply {
+                val circle = Rectangle(pos.x, pos.y, 120.0, 120.0).apply {
                     fill = grey
                     stroke = Color.WHITE
-                    strokeWidth = 1.0
+                    strokeWidth = 2.0
                 }
+
                 root.children.add(circle)
             }
         }
