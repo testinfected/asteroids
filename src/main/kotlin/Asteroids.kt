1f969a04350038d5f0277e096670b52e55712e4f
testinfected <vtence@gmail.com>
2021-04-19 23:38:13 -0400

    Draw using the Canvas rather than using a scene graph
    So that we can appreciate the difference. I like the Canvas
    better for now.

--- src/main/kotlin/Asteroids.kt
+++ src/main/kotlin/Asteroids.kt
@@ -2,12 +2,10 @@ import javafx.animation.AnimationTimer
 import javafx.application.Application
 import javafx.geometry.Point2D
 import javafx.scene.Group
-import javafx.scene.Node
 import javafx.scene.Scene
+import javafx.scene.canvas.Canvas
 import javafx.scene.paint.Color
 import javafx.scene.paint.Paint
-import javafx.scene.shape.Polygon
-import javafx.scene.shape.Rectangle
 import javafx.scene.transform.Rotate
 import javafx.stage.Stage
 import kotlin.random.Random
@@ -25,40 +23,19 @@ class Asteroids : Application() {
 
     override fun start(stage: Stage) {
         stage.title = "Asteroids"
-        val (actors, scene) = set(stage)
 
-        fun clearScene() {
-            actors.children.clear()
-        }
-
-        fun add(actor: Node) {
-            actors.children.add(actor)
-        }
-
-        val ship = makeShip(scene)
-        val asteroids = (1..10).map { makeAsteroid(scene) }.toList()
+        val canvas = set(stage)
+        val ship = makeShip(canvas)
+        val asteroids = (1..10).map { makeAsteroid(canvas) }.toList()
 
         val timer = object : AnimationTimer() {
             override fun handle(now: Long) {
-                clearScene()
-                add(drawShip(scene))
-                add(drawAsteroids(scene))
-            }
-
-            private fun drawAsteroids(scene: Scene): Node {
-                val group = Group()
-                for (asteroid in asteroids) {
-                    val shape = draw(asteroid)
-                    move(asteroid, scene)
-                    group.children.add(shape)
-                }
-                return group
-            }
-
-            private fun drawShip(scene: Scene): Node {
-                val shape = draw(ship)
+                save(canvas)
+                clear(canvas)
+                draw(ship, canvas)
                 move(ship)
-                return shape
+                draw(asteroids, canvas)
+                restore(canvas)
             }
         }
 
@@ -66,50 +43,85 @@ class Asteroids : Application() {
         stage.show()
     }
 
+    private fun set(stage: Stage): Canvas {
+        val canvas = Canvas(800.0, 600.0)
+        val scene = Scene(Group(canvas))
+        stage.scene = scene
+        return canvas
+    }
+
+    fun clear(canvas: Canvas) {
+        val graphics = canvas.graphicsContext2D.apply {
+            fill = grey
+        }
+
+        graphics.fillRect(0.0, 0.0, canvas.width, canvas.height)
+    }
+
+    private fun restore(canvas: Canvas) {
+        canvas.graphicsContext2D.restore()
+    }
+
+    private fun save(canvas: Canvas) {
+        canvas.graphicsContext2D.save()
+    }
+
     private fun move(ship: Ship) {
         ship.angle += 1
     }
 
-    private fun set(stage: Stage): Pair<Group, Scene> {
-        val graph = Group()
-        val scene = Scene(graph, 800.0, 600.0, grey)
-        stage.scene = scene
-        return Pair(graph, scene)
-    }
+    private fun makeShip(canvas: Canvas) = Ship(Point2D.ZERO.midpoint(Point2D(canvas.width, canvas.height)))
 
-    private fun makeShip(scene: Scene) = Ship(Point2D.ZERO.midpoint(Point2D(scene.width, scene.height)))
+    private fun draw(ship: Ship, canvas: Canvas) {
+        val graphics = canvas.graphicsContext2D
+        graphics.save()
 
-    private fun draw(ship: Ship): Polygon {
-        val shape = Polygon(
-            10.0, 0.0,
-            -10.0, 6.0,
-            -10.0, -6.0
-        ).apply {
-            layoutX = ship.pos.x
-            layoutY = ship.pos.y
+        graphics.apply {
             fill = Color.TRANSPARENT
             stroke = Color.WHITE
-            strokeWidth = 2.0
+            lineWidth = 2.0
         }
-        shape.transforms.add(Rotate(ship.angle))
-        return shape
+
+        val dx = 10.0
+        val dy = 6.0
+
+        graphics.translate(ship.pos.x, ship.pos.y)
+        graphics.rotate(ship.angle)
+        graphics.strokeLine(dx, 0.0, -dx, dy)
+        graphics.strokeLine(-dx, dy, -dx, -dy)
+        graphics.strokeLine(-dx, - dy, dx, 0.0)
+
+        graphics.restore()
     }
 
-    private fun makeAsteroid(scene: Scene) = Asteroid(
-        Point2D(Random.nextDouble() * scene.width, Random.nextDouble() * scene.height),
+    private fun makeAsteroid(canvas: Canvas) = Asteroid(
+        Point2D(Random.nextDouble() * canvas.width, Random.nextDouble() * canvas.height),
         Random.nextDouble().times(360)
     )
 
-    private fun draw(asteroid: Asteroid): Rectangle {
-        return Rectangle(asteroid.pos.x - 30.0, asteroid.pos.y - 30.0, 60.0, 60.0).apply {
-            fill = Color.TRANSPARENT
-            stroke = Color.WHITE
-            strokeWidth = 2.0
+    private fun draw(asteroids: List<Asteroid>, canvas: Canvas) {
+        for (asteroid in asteroids) {
+            draw(asteroid, canvas)
+            move(asteroid, canvas)
         }
     }
 
-    private fun move(asteroid: Asteroid, scene: Scene) {
-        asteroid.pos = warp(step(asteroid), Point2D(scene.width, scene.height))
+    private fun draw(asteroid: Asteroid, canvas: Canvas) {
+        val graphics = canvas.graphicsContext2D
+        graphics.save()
+
+        graphics.apply {
+            fill = Color.TRANSPARENT
+            stroke = Color.WHITE
+            lineWidth = 2.0
+        }
+        graphics.strokeRect(asteroid.pos.x - 30.0, asteroid.pos.y - 30.0, 60.0, 60.0)
+
+        graphics.restore()
+    }
+
+    private fun move(asteroid: Asteroid, canvas: Canvas) {
+        asteroid.pos = warp(step(asteroid), Point2D(canvas.width, canvas.height))
     }
 
     private fun step(asteroid: Asteroid): Point2D {
